b3b06b43fcec46d222f96d0e65912ac239c7a070
DevJaimeGR <dev.backend.jigr@hotmail.com>
2024-12-27 23:28:58 -0500

    Add Maui-CodeQuotes project

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUI.Project.CodeQuotes
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
